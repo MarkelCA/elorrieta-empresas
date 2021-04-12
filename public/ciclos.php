7e4fa52957b1b8da43f8afa9327133f8da2e6068
Markel <cuestaarribas.markel@gmail.com>
2021-04-12 17:54:51 +0200

    refactoring, remaking clean-urls

--- public/ciclos.php
+++ public/ciclos.php
@@ -7,12 +7,12 @@
     <meta name="keywords" content="empresa, practicas, ciclos, dual, FCT, Elorrieta"/>
     <meta name="author" content="Markel Cuesta" />
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="shortcut icon" href="./src/img/favicon-black.ico" />
+     <link rel="shortcut icon" href="../src/img/favicon-black.ico" />
 
      <!--Font awesome-->
-     <script defer src="./modules/font-awesome/font-awesome.js" crossorigin="anonymous"></script>
+     <script defer src="../modules/font-awesome/font-awesome.js" crossorigin="anonymous"></script>
     <!--JQuery-->
-    <script defer src="./modules/jquery/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script defer src="../modules/jquery/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
     
     <!--JQuery UI-->
@@ -24,16 +24,16 @@
   <title>Ciclos - Elorrieta Empresas</title>
 
     <!--Animate.css-->
-    <link rel="stylesheet" href="./modules/animate.css/animate.min.css" />
+    <link rel="stylesheet" href="../modules/animate.css/animate.min.css" />
     <!--Estilos propios-->
-    <link rel="stylesheet" type="text/css" href="./src/estilos/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="../src/estilos/css/all.min.css">
     <!--Slick-->
-    <link rel="stylesheet" type="text/css" href="./modules/slick-1.8.1/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="./modules/slick-1.8.1/slick-theme.css"/>
-    <script defer type="text/javascript" src="./modules/slick-1.8.1/slick.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../modules/slick-1.8.1/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="../modules/slick-1.8.1/slick-theme.css"/>
+    <script defer type="text/javascript" src="../modules/slick-1.8.1/slick.min.js"></script>
 
     <!--Funciones php-->
-    <?php require_once 'funciones.php';?>
+    <?php require_once '../funciones.php';?>
 
     <!--Activar carrousel-->
     <script defer src='js/ciclos.js'></script>
