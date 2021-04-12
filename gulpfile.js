bb6d5d5b090927ff76731c47f56d6d4c73c9ce4c
Markel <cuestaarribas.markel@gmail.com>
2021-04-12 16:40:19 +0200

    backup before project structure refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat')
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
 
 
 sass.compiler = require('node-sass');
@@ -14,8 +16,10 @@ watch('./src/estilos/scss/**/*.scss', watch_sass);
 
 function watch_sass() {
  return gulp.src('./src/estilos/scss/**/*.scss')
+  .pipe(imagemin())
   .pipe(concat('../scss/all.min.css'))
   .pipe(sourcemaps.init())
+  .pipe(autoprefixer())
   .pipe(sass().on('error', sass.logError))
   .pipe(sourcemaps.write('./maps'))
   .pipe(cleanCSS())
